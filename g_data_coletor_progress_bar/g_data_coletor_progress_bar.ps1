1b8e749830d03531d0ec107663004539172a3393
Rafael Lisboa <rafael.lisboa0@gmail.com>
2020-10-01 12:07:50 -0300

    first_script_load

---
+++ g_data_coletor_progress_bar/g_data_coletor_progress_bar.ps1
@@ -0,0 +1,30 @@
+﻿##NOTE: This is a generic script. Feel free to modify and adapt to your necessities.
+
+$rand = Get-Random -Minimum 10 -Maximum 20
+
+# This FOR and Write-Progress function insert a nice progress bar during script execution
+for ($i = $rand; $i -le 100; $i=$i+20 )
+{
+	Write-Progress -Activity "IMPUT YOUR MSG HERE" -Status "$i% IMPUT YOUR MSG HERE" -PercentComplete $i;
+
+$lines = Get-Content -Path "C:\IMPUT\YOUR_LOCAL\DIR_HERE*" -Filter *.log | select-string "IMPUT:","YOUR:","DATA:","HERE:","FROM:","FILES:","THAT_YOU:","WANT:","EXTRACT:",| %{$_ -replace '$',''}
+    
+}
+# Number 9 means 9 categories. Did you get it?
+$result = for ($i = 0; $i -lt $lines.Count; $i += 9) {
+    $str = (($lines[$i] -split ':', 2).Trim() -join ' = '),
+           (($lines[$i + 1] -split ':', 2).Trim() -join ' = '),
+           (($lines[$i + 2] -split ':', 2).Trim() -join ' = '),
+           (($lines[$i + 3] -split ':', 2).Trim() -join ' = '),
+           (($lines[$i + 4] -split ':', 2).Trim() -join ' = '),
+           (($lines[$i + 5] -split ':', 2).Trim() -join ' = '),
+           (($lines[$i + 6] -split ':', 2).Trim() -join ' = '),
+           (($lines[$i + 7] -split ':', 2).Trim() -join ' = '),
+           (($lines[$i + 8] -split ':', 2).Trim() -join ' = ') -join "`r`n"
+		   
+# Just repeat all categories, without ":" 
+	[PsCustomObject]($str | ConvertFrom-StringData) | Select-Object 'IMPUT','YOUR','DATA','HERE','FROM','FILES','THAT_YOU','WANT','EXTRACT'
+} 
+# This will put all data into a single CSV file
+$result | Export-Csv -Path .\YOUR_NEM_FILE.csv -Delimiter ';' -NoTypeInformation
+
